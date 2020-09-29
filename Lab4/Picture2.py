e32f07c8f494385e93b9d5b2faa424daff145319
amansyayf <siaifetdinov.ar@phystech.edu>
2020-09-29 16:26:10 +0300

    change

--- Lab4/Picture2.py
+++ Lab4/Picture2.py
@@ -4,7 +4,8 @@ import numpy as np
 
 pg.init()
 
-BLACK=(0,0,0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 process = True
 sc_width = 1000
@@ -24,6 +25,12 @@ pg.draw.rect(sc, (236, 205, 0), (int(sc_width * 0), 0, 40, 440))
 
 # Создаем функцию, создающую ежа
 def hedgehog(x, y):
+    """
+
+    :param x:
+    :param y:
+    :return:
+    """
     a = hedgehog_width / 2.3
     b = hedgehog_height / 2.3
     pg.draw.ellipse(sc, (68, 0, 19), (sc_width * x, sc_height * y, hedgehog_width, hedgehog_height))
@@ -31,20 +38,20 @@ def hedgehog(x, y):
 
     pg.draw.ellipse(sc, (68, 0, 19),
                     (sc_width * x + hedgehog_width * 0.9, sc_height * y + hedgehog_height * 0.4, 50, 30))
-    pg.draw.ellipse(sc, (0, 0, 0), (sc_width * x + hedgehog_width * 0.9, sc_height * y + hedgehog_height * 0.4, 50, 30),
-                    1)
+    pg.draw.ellipse(sc, (0, 0, 0),
+                    (sc_width * x + hedgehog_width * 0.9, sc_height * y + hedgehog_height * 0.4, 50, 30),1)
 
-    pg.draw.ellipse(sc, (0, 0, 0),
+    pg.draw.ellipse(sc, BLACK,
                     (sc_width * x + hedgehog_width + 13, sc_height * y + hedgehog_height * 0.4 + 6, 8, 8))
-    pg.draw.ellipse(sc, (0, 0, 0),
+    pg.draw.ellipse(sc, BLACK,
                     (sc_width * x + hedgehog_width + 2, sc_height * y + hedgehog_height * 0.4 + 10, 8, 8))
-    pg.draw.ellipse(sc, (0, 0, 0),
+    pg.draw.ellipse(sc, BLACK,
                     (sc_width * x + hedgehog_width + 23, sc_height * y + hedgehog_height * 0.4 + 11, 6, 6))
-    pg.draw.ellipse(sc, (255, 255, 255),
+    pg.draw.ellipse(sc, WHITE,
                     (sc_width * x + hedgehog_width + 13, sc_height * y + hedgehog_height * 0.4 + 6, 8, 8), 1)
-    pg.draw.ellipse(sc, (255, 255, 255),
+    pg.draw.ellipse(sc, WHITE,
                     (sc_width * x + hedgehog_width + 2, sc_height * y + hedgehog_height * 0.4 + 10, 8, 8), 1)
-    pg.draw.ellipse(sc, (255, 255, 255),
+    pg.draw.ellipse(sc, WHITE,
                     (sc_width * x + hedgehog_width + 23, sc_height * y + hedgehog_height * 0.4 + 11, 6, 6), 1)
 
     pg.draw.ellipse(sc, (68, 0, 19), (sc_width * x, sc_height * y + hedgehog_height - 20, 40, 20))
@@ -57,11 +64,11 @@ def hedgehog(x, y):
         x0 = random.randint(-int(a), int(a))
         y0 = random.randint(-int(b * (1 - (x0 / a) ** 2) ** 0.5), int(b * (1 - (x0 / a) ** 2) ** 0.5))
         k = random.uniform(-np.pi / 5, np.pi / 5)
-        t = [sc_width * x + a + x0, sc_height * y + b + y0]
-        B = [sc_width * x + a + x0 + 20 * np.cos(k), sc_height * y + b + y0 - 20 * np.sin(k)]
-        C = [sc_width * x + a + x0 + 5 - 90 * np.sin(k), sc_height * y + b + y0 - 90 * np.cos(k)]
-        pg.draw.polygon(sc, (30, 30, 30), [t, B, C], )
-        pg.draw.polygon(sc, (0, 0, 0), [t, B, C], 1)
+        ver_1 = [sc_width * x + a + x0, sc_height * y + b + y0]
+        ver_2 = [sc_width * x + a + x0 + 20 * np.cos(k), sc_height * y + b + y0 - 20 * np.sin(k)]
+        ver_3 = [sc_width * x + a + x0 + 5 - 90 * np.sin(k), sc_height * y + b + y0 - 90 * np.cos(k)]
+        pg.draw.polygon(sc, (30, 30, 30), [ver_1, ver_2, ver_3], )
+        pg.draw.polygon(sc, (0, 0, 0), [ver_1, ver_2, ver_3], 1)
 
     pg.draw.ellipse(sc, (255, 33, 0),
                     (sc_width * x + hedgehog_width - 100, sc_height * y + hedgehog_height - 140, 70, 70))
@@ -72,13 +79,13 @@ def hedgehog(x, y):
         x0 = random.randint(-int(a), int(a))
         y0 = random.randint(-int(b * (1 - (x0 / a) ** 2) ** 0.5), int(b * (1 - (x0 / a) ** 2) ** 0.5))
         k = random.uniform(-np.pi / 5, np.pi / 5)
-        t = [sc_width * x + a + x0, sc_height * y + b + y0]
-        B = [sc_width * x + a + x0 + 20 * np.cos(k), sc_height * y + b + y0 - 20 * np.sin(k)]
-        C = [sc_width * x + a + x0 + 5 - 90 * np.sin(k), sc_height * y + b + y0 - 90 * np.cos(k)]
-        pg.draw.polygon(sc, (30, 30, 30), [t, B, C], )
-        pg.draw.polygon(sc, (0, 0, 0), [t, B, C], 1)
+        ver_1 = [sc_width * x + a + x0, sc_height * y + b + y0]
+        ver_2 = [sc_width * x + a + x0 + 20 * np.cos(k), sc_height * y + b + y0 - 20 * np.sin(k)]
+        ver_3 = [sc_width * x + a + x0 + 5 - 90 * np.sin(k), sc_height * y + b + y0 - 90 * np.cos(k)]
+        pg.draw.polygon(sc, (30, 30, 30), [ver_1, ver_2, ver_3], )
+        pg.draw.polygon(sc, (0, 0, 0), [ver_1, ver_2, ver_3], 1)
 
-    pg.draw.ellipse(sc, (255, 255, 255),
+    pg.draw.ellipse(sc, WHITE,
                     (sc_width * x + hedgehog_width - 140, sc_height * y + hedgehog_height - 160, 20, 70))
     pg.draw.ellipse(sc, (255, 127, 116),
                     (sc_width * x + hedgehog_width - 175, sc_height * y + hedgehog_height - 160, 90, 30))
@@ -87,22 +94,18 @@ def hedgehog(x, y):
         x0 = random.randint(-int(a), int(a))
         y0 = random.randint(-int(b * (1 - (x0 / a) ** 2) ** 0.5), int(b * (1 - (x0 / a) ** 2) ** 0.5))
         k = random.uniform(-np.pi / 5, np.pi / 5)
-        t = [sc_width * x + a + x0, sc_height * y + b + y0]
-        B = [sc_width * x + a + x0 + 20 * np.cos(k), sc_height * y + b + y0 - 20 * np.sin(k)]
-        C = [sc_width * x + a + x0 + 5 - 90 * np.sin(k), sc_height * y + b + y0 - 90 * np.cos(k)]
-        pg.draw.polygon(sc, (30, 30, 30), [t, B, C], )
-        pg.draw.polygon(sc, (0, 0, 0), [t, B, C], 1)
+        ver_1 = [sc_width * x + a + x0, sc_height * y + b + y0]
+        ver_2 = [sc_width * x + a + x0 + 20 * np.cos(k), sc_height * y + b + y0 - 20 * np.sin(k)]
+        ver_3 = [sc_width * x + a + x0 + 5 - 90 * np.sin(k), sc_height * y + b + y0 - 90 * np.cos(k)]
+        pg.draw.polygon(sc, (30, 30, 30), [ver_1, ver_2, ver_3], )
+        pg.draw.polygon(sc, BLACK, [ver_1, ver_2, ver_3], 1)
 
 
 hedgehog(0.6, 0.7)
-hedgehog_width = 100
-hedgehog_height = 50
 hedgehog(0, 0.7)
 pg.display.update()
 
 while process:
-    # hedgehog(0.6, 0.7)
-    # pg.display.update()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             process = False
